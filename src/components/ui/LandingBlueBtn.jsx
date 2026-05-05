c19755de20fc2fa122069c88808cf2eb06f582ea
Ayush Barman <wwwanotoniomontana001@gmail.com>
2026-05-05 15:07:17 +0530

    fixed landing page button for mobile devices

--- src/components/ui/LandingBlueBtn.jsx
+++ src/components/ui/LandingBlueBtn.jsx
@@ -5,7 +5,7 @@ const LandingBlueBtn = ({ text, LinkTo }) => {
   return (
     <Link
       to={LinkTo}
-      className="active:ring-1 border border-solid border-blue-500 font-semibold text-blue-700 rounded-full px-6 py-3 w-fit transition-all duration-200 ease-in-out hover:bg-blue-100 hover:text-blue-800"
+      className="active:ring-1 border border-solid border-blue-500 font-semibold text-blue-700 rounded-full md:px-6 px-4 md:py-3 py-2 w-fit transition-all duration-200 ease-in-out hover:bg-blue-100 hover:text-blue-800"
     >
       {text}
     </Link>
